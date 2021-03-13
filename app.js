4d3dae81ce82937626eaf6c83619c0a697f50084
karinalitwynenko <k.litvynenko@gmail.com>
2021-03-13 16:03:19 +0100

    Add manufacturer statistics

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 
 
 const PORT = 3000;
-const {COLUMNS} = require('./columns');
+const {COLUMNS, MANUFACTURER} = require('./columns');
+const FILE = 'katalog-test.txt';
 
 eval(fs.readFileSync('text-file-reader.js') + '');
 
@@ -16,13 +17,13 @@ app.listen(PORT, function(error){
     if(error) {
         throw error;
     }
-    // else {
-    //     printTable(load());
-    // }
+    else {
+        printTable(load());
+    }
 });
 
 app.get('/katalog.txt', function(req, res) {
-    res.sendFile(path.resolve('./katalog.txt'));
+    res.sendFile(path.resolve('./' + FILE));
 });
 
 app.get('/', (req, res) => {
@@ -30,7 +31,8 @@ app.get('/', (req, res) => {
 });
 
 app.get('/katalog', (req, res) => {
-    res.render('katalog', { data: csvToMap(load()) });
+    let table = csvToMap(load());
+    res.render('katalog', { data: table, manufacturers: getManufacturerStats(table) });
 });
 
 app.post('/save', (req, res) => {
@@ -38,7 +40,7 @@ app.post('/save', (req, res) => {
 });
 
 function load() {
-    return fs.readFileSync('katalog-test.txt', 'utf8', function (err, data) {
+    return fs.readFileSync(FILE, 'utf8', function (err, data) {
 		if (err) {
 		    console.log(err);
 		}
@@ -64,7 +66,7 @@ function writeToFile(data) {
         }
     }
 
-    fs.writeFile('katalog-test.txt', textData, () => console.log('Data saved'));
+    fs.writeFile(FILE, textData, () => console.log('Data saved'));
 }
 
 function csvToMap(csv) {
@@ -74,13 +76,12 @@ function csvToMap(csv) {
     for(let i = 0; i < rows.length + 1; i++) {
         row = [];
         if(i == 0) {
-            row[0] = 'Lp.';
-            row = row.concat(COLUMNS);
+            row = COLUMNS;
             table.set(i, row);
         }
         else {
             row[0] = i + '';
-            row = row.concat(rows[i - 1].split(';', COLUMNS.length));
+            row = row.concat(rows[i - 1].split(';', COLUMNS.length - 1));
             row.forEach(item => item.trim());
             table.set(i, row);
         }
@@ -88,4 +89,27 @@ function csvToMap(csv) {
     }
 
     return table;
+}
+
+function getManufacturerStats(table) {
+    let manufacturers = new Map();
+    manufacturers.set('header', ['Producent', 'Liczba laptopów']);
+    let isFirst = true;
+
+    table.forEach(row => {
+        if(isFirst) {
+            isFirst = false;
+            return;
+        }
+
+        if(manufacturers.get(row[MANUFACTURER]) === undefined) {
+            manufacturers.set(row[MANUFACTURER], 1);
+        }
+        else {
+            manufacturers.set(row[MANUFACTURER], manufacturers.get(row[MANUFACTURER]) + 1);
+        }
+    });
+
+    console.log(manufacturers)
+    return manufacturers;
 }
--- columns.js
+++ columns.js
@@ -1,4 +1,6 @@
-const COLUMNS = 'nazwa producenta; przekątna ekranu; rozdzielczość ekranu; rodzaj powierzchni ekranu; czy ekran jest dotykowy; nazwa procesora; liczba rdzeni fizycznych; prędkość taktowania MHz; wielkość pamięci RAM; pojemność dysku; rodzaj dysku; nazwa układu graficznego; pamięć układu graficznego; nazwa systemu operacyjnego; rodzaj napędu fizycznego w komputerze'.split(';');
+const MANUFACTURER = 1;
+const COLUMNS = 'Lp.; nazwa producenta; przekątna ekranu; rozdzielczość ekranu; rodzaj powierzchni ekranu; czy ekran jest dotykowy; nazwa procesora; liczba rdzeni fizycznych; prędkość taktowania MHz; wielkość pamięci RAM; pojemność dysku; rodzaj dysku; nazwa układu graficznego; pamięć układu graficznego; nazwa systemu operacyjnego; rodzaj napędu fizycznego w komputerze'.split(';');
 module.exports = {
-    COLUMNS
+    COLUMNS,
+    MANUFACTURER
 }
